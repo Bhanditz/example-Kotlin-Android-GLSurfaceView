9f7e8436429da3e35d0b3795be911327f463ecee
egslava@gmail.com <egslava@gmail.com>
2016-03-01 13:49:55 +0200

    [add]OpenGL-related code

--- app/src/main/java/ru/egslava/tutorialglsurfaceview/MainActivity.kt
+++ app/src/main/java/ru/egslava/tutorialglsurfaceview/MainActivity.kt
@@ -1,12 +1,37 @@
 package ru.egslava.tutorialglsurfaceview
 
+import android.opengl.GLES20.GL_COLOR_BUFFER_BIT
+import android.opengl.GLSurfaceView
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        glSurface.setRenderer(TutorialRenderer())
+    }
+
+    class TutorialRenderer : GLSurfaceView.Renderer {
+        var color = 0f
+        var colorVelocity = 0.05f
+
+        override fun onDrawFrame(gl: GL10){
+            if (color > 1 || color < 0){
+                colorVelocity = -colorVelocity
+            }
+            color += colorVelocity
+
+            gl.glClearColor(color * 0.5f, color, color, 1f)
+            gl.glClear(GL_COLOR_BUFFER_BIT)
+        }
+
+        override fun onSurfaceCreated(gl: GL10, config: EGLConfig){}
+        override fun onSurfaceChanged(gl: GL10, width: Int, height: Int){}
     }
 }
